f78407ac491aaa4b9e028ad4758e9487896a11f0
NikhilAg2021 <78316627+NikhilAg2021@users.noreply.github.com>
2021-06-14 13:16:13 +0530

    Add yaw file - controls the yaw of the drone

---
+++ yaw.py
@@ -0,0 +1,25 @@
+from dronekit import VehicleMode, connect
+import time
+
+from pymavlink import mavutil
+
+vehicle=connect('127.0.0.1:14550',wait_ready=True)
+
+def condition_yaw(heading, relative=False): #Allows to specify whether heading is absolute or value is relative to previously set heading 
+
+    if relative:
+        is_relative=1 #yaw relative to direction of travel
+    else:
+        is_relative=0 #yaw is an absolute angle
+    # create the CONDITION_YAW command using command_long_encode()
+    msg = vehicle.message_factory.command_long_encode(
+        0, 0,    # target system, target component
+        mavutil.mavlink.MAV_CMD_CONDITION_YAW, #command
+        0, #confirmation
+        heading,    # param 1, yaw in degrees
+        0,          # param 2, yaw speed deg/s
+        1,          # param 3, direction -1 ccw, 1 cw?????Meaning?
+        is_relative, # param 4, relative offset 1, absolute angle 0
+        0, 0, 0)    # param 5 ~ 7 not used
+    # send command to vehicle
+    vehicle.send_mavlink(msg)
